56ef28a12063d389d98ff18cc69861bbd8d3f2ff
James Osborn <jamescarlosborn@gmail.com>
2018-01-19 14:51:27 -0800

    update Namespace names, add appsettings.json

--- jco-portfolio/Models/ApplicationDbContext.cs
+++ jco-portfolio/Models/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BasicAuthentication.Models
+namespace Portfolio.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
--- jco-portfolio/Models/ApplicationUser.cs
+++ jco-portfolio/Models/ApplicationUser.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace jco-portfolio.Models
+namespace Portfolio.Models
 {
-    public class Class
+    public class ApplicationUser : IdentityUser
     {
+
     }
 }
