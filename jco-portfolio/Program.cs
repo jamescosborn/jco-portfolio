49a3d1fcef463dbcdeabce9f3c83907934d7378b
James Osborn <jamescarlosborn@gmail.com>
2018-01-19 15:25:06 -0800

    fix error in Program.cs

--- jco-portfolio/Program.cs
+++ jco-portfolio/Program.cs
@@ -16,7 +16,6 @@ namespace Portfolio
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
