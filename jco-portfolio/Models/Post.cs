4fd0c6d56f8c9f5aff0604344d3567ea6f05facc
James Osborn <jamescarlosborn@gmail.com>
2018-01-21 23:15:39 -0800

    update Models/Post.cs

--- jco-portfolio/Models/Post.cs
+++ jco-portfolio/Models/Post.cs
@@ -10,10 +10,16 @@ namespace Portfolio.Models
     [Table("Posts")]
     public class Post
     {
+        public Post()
+        {
+            this.Comments = new HashSet<Comment>();
+        }
+
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public virtual ApplicationUser User { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
