1bcb67a0a08067f8589e6254d8b16458c04ecec3
James Osborn <jamescarlosborn@gmail.com>
2018-01-21 20:59:50 -0800

    add Create to PostController

--- jco-portfolio/Controllers/PostController.cs
+++ jco-portfolio/Controllers/PostController.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Portfolio.Models;
+using Microsoft.AspNetCore.Identity;
 
-namespace jco-portfolio.Controllers
+namespace Portfolio.Controllers
 {
-    public class PostController
+    [Authorize]
+    public class PostController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Item item)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            item.User = currentUser;
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
