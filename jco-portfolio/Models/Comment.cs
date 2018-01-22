120c1d9ffdad3c13761324521c59448fc629d387
James Osborn <jamescarlosborn@gmail.com>
2018-01-21 23:05:52 -0800

    add Models/Comment.cs

---
+++ jco-portfolio/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portfolio.Models
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public int PostId { get; set; }
+        public virtual Post post { get; set; }
+    }
+}
