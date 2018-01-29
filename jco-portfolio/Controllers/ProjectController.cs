c332886644385cb10127fdc7fdb619e378936fb4
James Osborn <jamescarlosborn@gmail.com>
2018-01-28 22:52:13 -0800

    add ProjectController.cs

---
+++ jco-portfolio/Controllers/ProjectController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
